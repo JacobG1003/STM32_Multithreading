afae1a571b6fc3a964d2122c34e75991d7e8cf2b
Jacob Gordon <44929491+JacobG1003@users.noreply.github.com>
2024-05-19 17:47:23 -0400

    Added more documentation to Kernel.C

--- Core/Src/kernel.c
+++ Core/Src/kernel.c
@@ -1,8 +1,10 @@
 /*
- * kernel.c
- *
- *  Created on: Oct 18, 2023
+ * 
+ *		Program Name: kernel.c
+ *  	Created on: Oct 18, 2023
  *      Author: Jacob Gordon
+ *		Description: Handles kernel actions including pending context switches, starting the kernel, managing service calls, handling scheduling and creating threads
+ * 		Created for MTE 241: Introduction to Real Time Systems
  */
 
 #include <stdio.h>
@@ -15,30 +17,27 @@ thread threadArray[5];
 uint32_t currentThread;
 uint32_t software_timer = 1000;
 int started = 0;
+
 void SVC_Handler_Main( unsigned int *svc_args )
 {
 	unsigned int svc_number;
 	/*
-	* Stack contains:
-	* r0, r1, r2, r3, r12, r14, the return address and xPSR
+	* Stack has r0, r1, r2, r3, r12, r14, the return address and xPSR
 	* First argument (r0) is svc_args[0]
 	*/
 	svc_number = ( ( char * )svc_args[ 6 ] )[ -2 ] ;
 	switch( svc_number )
 	{
 	case 0:
-			__set_PSP((int32_t)threadArray[0].sp);
-			runFirstThread();
-			break;
+		__set_PSP((int32_t)threadArray[0].sp);
+		runFirstThread();
+		break;
 	case 1:
 		printf("Success!\r\n");
 		break;
 	case 2:
-			printf("Fail!\r\n");
-			break;
-	case 3:
-			printf("Other?\r\n");
-			break;
+		printf("Fail!\r\n");
+		break;
 	case 4:
 		//Pend an interrupt to do the context switch
 		_ICSR |= 1<<28;
@@ -49,17 +48,22 @@ void SVC_Handler_Main( unsigned int *svc_args )
 	}
 }
 
+//	Function returns available memory address for next thread and increment thread count
 uint32_t* findThread()
 {
-	uint32_t newAdress = MSP_INIT_VAL - (threadCount+1)*THREAD_SIZE;
+	uint32_t newAddress = MSP_INIT_VAL - (threadCount+1)*THREAD_SIZE;
 
-	if(newAdress < (MSP_INIT_VAL - MAX_STACK_SIZE - THREAD_SIZE))
+	if(newAddress < (MSP_INIT_VAL - MAX_STACK_SIZE - THREAD_SIZE))
 		return (uint32_t*)(0x0);
 	threadCount++;
-	return (uint32_t *)newAdress;
+	return (uint32_t *)newAddress;
 }
 
-
+/*
+	Basic Thread Creating Function
+	Takes in a function pointer for the thread
+	Returns 1 on success and 0 on fail
+*/
 char osCreateThread(void (*func_ptr)(void*))
 {
 	uint32_t* thread_ptr = findThread();
@@ -71,7 +75,8 @@ char osCreateThread(void (*func_ptr)(void*))
 	threadArray[threadNum].timeslice = RRT;
 	threadArray[threadNum].runtime = RRT;
 
-	*(--(threadArray[threadNum].sp)) = 1<<24; //A magic number, this is xPSR
+	//Setup for new thread registers
+	*(--(threadArray[threadNum].sp)) = 1<<24; //xPSR
 	*(--(threadArray[threadNum].sp)) = (uint32_t)func_ptr; //PC
 	*(--threadArray[threadNum].sp) = 0xA; //LR
 	*(--threadArray[threadNum].sp) = 0xA; //R12
@@ -86,7 +91,11 @@ char osCreateThread(void (*func_ptr)(void*))
 
 	return 1;
 }
-
+/*
+	Thread Creating Function with custom timeout and thread args
+	Takes in a function pointer for the thread, timeout number and a void ptr for args
+	Returns 1 on success and 0 on fail
+*/
 char osCreateThreadArgs(void (*func_ptr)(void*), uint32_t timeout, void* args)
 {
 	uint32_t* thread_ptr = findThread();
@@ -98,8 +107,8 @@ char osCreateThreadArgs(void (*func_ptr)(void*), uint32_t timeout, void* args)
 	threadArray[threadNum].timeslice = timeout;
 	threadArray[threadNum].runtime = timeout;
 
-
-	*(--(threadArray[threadNum].sp)) = 1<<24; //A magic number, this is xPSR
+	//Setup for new thread registers
+	*(--(threadArray[threadNum].sp)) = 1<<24; //xPSR
 	*(--(threadArray[threadNum].sp)) = (uint32_t)func_ptr; //PC
 	*(--threadArray[threadNum].sp) = 0xA; //LR
 	*(--threadArray[threadNum].sp) = 0xA; //R12
@@ -115,14 +124,21 @@ char osCreateThreadArgs(void (*func_ptr)(void*), uint32_t timeout, void* args)
 	return 1;
 }
 
+/*
+	Initializes the kernel 
+	Sets the priority of interuppts
+*/
 void osKernelInitialize()
 {
 	MSP_INIT_VAL = (uint32_t)(*(uint32_t**)0x0);
 	threadCount = 0;
-	SHPR3 |= 0xFE << 16; //shift the constant 0xFE 16 bits to set PendSV priority
+	SHPR3 |= 0xFE << 16; //shift 0xFE 16 bits to set PendSV priority
 	SHPR2 |= 0xFDU << 24; //Set the priority of SVC higher than PendSV
 }
 
+/*
+	Round robin scheduler, saves current threads sp then sets the psp to new thread
+*/
 void osSched()
 {
 	threadArray[currentThread].sp = (uint32_t*)(__get_PSP() - 8*4);
@@ -131,18 +147,24 @@ void osSched()
 	return;
 }
 
-
+//	Function to start the kernel, makes a service call
 void osKernelStart()
 {
 	started = 1;
 	__asm("SVC #0");
 }
+
+//	Function for a thread to yield, resets the runtime
 void osYield(void)
 {
 	threadArray[currentThread].runtime = threadArray[currentThread].timeslice;
 	__asm("SVC #4");
 }
 
+/*
+	Function to preemts a thread to yield
+	If the thread has started then decrements runtime and if 0 force yield
+*/
 void pre_emptive()
 {
 	if(started == 0) return;
